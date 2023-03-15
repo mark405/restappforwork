293e23b3232271ec4271d773298e2ae808ea6321
mark405 <mark200320031.tun@gmail.com>
2023-03-15 23:54:18 +0200

    initialize project

---
+++ src/test/java/zavgod/tech/Tech/TechApplicationTests.java
@@ -0,0 +1,13 @@
+package zavgod.tech.Tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
